c3a401ce36a78e80d6d2108c916a68578ec6d350
Anand <anandbrose@yahoo.com>
2017-10-12 09:20:06 +0530

    Initial Commit

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Camera.main.transform.forward*Input.GetAxis("Vertical")*Time.deltaTime*2f;
+        transform.position += Camera.main.transform.right*Input.GetAxis("Horizontal")*Time.deltaTime*2f;
+	}
+}
