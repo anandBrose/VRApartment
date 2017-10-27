13fc4c94bdb7f0e21c7cab3a752a23a31d93d846
Anand <anandbrose@yahoo.com>
2017-10-28 02:15:49 +0530

    Functionality for UI

---
+++ Assets/VisualModifier.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualModifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void SetColor()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    public void SetTexture(Texture texture)
+    {
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+    }
+}
