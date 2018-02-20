c61cc4ae3af90b848f562f621c9535dc14e71600
Anand <anandbrose@yahoo.com>
2018-02-20 09:17:56 +0530

    Resort changes

---
+++ Assets/PlaceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceScript : MonoBehaviour
+{
+    public GameObject UIs;
+    public GameObject placeList;
+    public GameObject loading;
+    private bool nowHighlighted = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && nowHighlighted)
+        {
+            UIs.SetActive(false);
+            placeList.SetActive(true);
+            this.nowHighlighted = false;
+        }
+
+    }
+
+    public void Entered(string data)
+    {
+        this.nowHighlighted = true;
+    }
+
+    public void Exit()
+    {
+        this.nowHighlighted = false;
+    }
+}
---
+++ Assets/SetSkyBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSkyBox : MonoBehaviour {
+
+    private bool nowHighlighted = false;
+    public Material material;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && nowHighlighted)
+        {
+            this.nowHighlighted = false;
+            RenderSettings.skybox = material;
+        }
+
+    }
+
+    public void Entered(string data)
+    {
+        this.nowHighlighted = true;
+    }
+
+    public void Exit()
+    {
+        this.nowHighlighted = false;
+    }
+}
